6f8e649a32e2218a12245939d78e86f44aee874c
Eren25 <l21590076@sjuanrio.tecnm.mx>
2025-02-11 15:46:36 -0600

    actualizacion

--- app.py
+++ app.py
@@ -2,8 +2,14 @@ from flask import Flask
 #crear isinstancia
 app = Flask(__name__)
 #ruta raiz
+
 @app.route('/')
 def hola_mundo():
     return 'Hola mundo'
+
+@app.route('/alumnos')
+def getAlumnos():
+    return 'Aqui van los alumnos'
+
 if __name__ == '__main__':
     app.run(debug=True)
